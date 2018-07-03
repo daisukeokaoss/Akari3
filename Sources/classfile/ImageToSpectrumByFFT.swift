d7a278a7a83a4efd7b7c0f98bdda6ec1f3d58f07
Daisuke Oka <daisukeokahassou@gmail.com>
2018-07-03 13:03:40 +0900

    process to implement FFT

---
+++ Sources/classfile/ImageToSpectrumByFFT.swift
@@ -0,0 +1,16 @@
+//
+//  ImageToSpectrumByFFT.swift
+//  EasyCamery
+//
+//  Created by 岡大輔 on 2018/07/03.
+//  Copyright © 2018年 koherent.org. All rights reserved.
+//
+
+import UIKit
+
+public class ImageToSpectrumByFFT: NSObject {
+    public class func ImageToSpectrunByFFTFunction(inputImage:Image<RGBA<UInt8>>)->Array<UInt64>
+    {
+        
+    }
+}
